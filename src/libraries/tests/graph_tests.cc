85c5af5b53f95019af9b5138601b626c1317df98
kimberfl <kimberfl@student.21-school.ru>
2026-01-18 17:40:59 +0300

    added simple tests for graph loader

---
+++ src/libraries/tests/graph_tests.cc
@@ -0,0 +1,41 @@
+#include <gtest/gtest.h>
+#include "graph.h"
+
+TEST(GraphLoader, ValidGraphLoadsSuccessfully) {
+  Graph graph;
+  EXPECT_TRUE(graph.LoadGraphFromFile("tests/data/valid_graph_1.txt"));
+}
+
+TEST(GraphLoader, NonSymmetricGraphFails) {
+  Graph graph;
+  EXPECT_FALSE(
+      graph.LoadGraphFromFile("tests/data/invalid_non_symmetric.txt"));
+}
+
+TEST(GraphLoader, NegativeWeightFails) {
+  Graph graph;
+  EXPECT_FALSE(
+      graph.LoadGraphFromFile("tests/data/invalid_negative_weight.txt"));
+}
+
+TEST(GraphLoader, EmptyGraphFails) {
+  Graph graph;
+  EXPECT_FALSE(
+      graph.LoadGraphFromFile("tests/data/invalid_empty_graph.txt"));
+}
+
+TEST(GraphLoader, TruncatedFileFails) {
+  Graph graph;
+  EXPECT_FALSE(
+      graph.LoadGraphFromFile("tests/data/invalid_truncated.txt"));
+}
+
+// TEST(GraphLoader, MatrixIsLoadedCorrectly) {
+//   Graph graph;
+//   ASSERT_TRUE(graph.LoadGraphFromFile("tests/data/valid_graph_1.txt"));
+
+//   const auto& m = graph.GetAdjacencyMatrix();
+//   ASSERT_EQ(m.size(), 3u);
+//   EXPECT_EQ(m[0][1], 1);
+//   EXPECT_EQ(m[1][2], 3);
+// }
